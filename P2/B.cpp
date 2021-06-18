d727841cbf56fd81fcd9671d4e22e071703f886b
Sou00 <69080602+Sou00@users.noreply.github.com>
2021-06-19 00:37:15 +0200

    Update B.cpp

--- P2/B.cpp
+++ P2/B.cpp
@@ -1,265 +1,537 @@
-//Igor Lonak
+//Igor Łonak
 
-int Counter = 0;
-int Display = 31;
+int i=0;
+int power=0;
+int first=1;
 
-int dodaj( int a, int b ){
-    int c;
-	int d;
+void Emplace (char* tab, int* set){
+    if(i==0)
+        *set=0;
 
-    if ( b == 0 ) return a;
+    if(*(tab+i)=='0' || *(tab+i)=='1' || *(tab+i)==' '){
 
-	c = a ^ b;
-    d = ( a & b ) << 1;
+        if(*(tab+i)=='1' || *(tab+i)=='0'){
+            int temp=1;
 
-	return dodaj( c, d );
+            if(*(tab+i)=='1') temp<<=16;
+            i++;
+            if(*(tab+i)=='1') temp<<=8;
+            i++;
+            if(*(tab+i)=='1') temp<<=4;
+            i++;
+            if(*(tab+i)=='1') temp<<=2;
+            i++;
+            if(*(tab+i)=='1') temp<<=1;
+
+
+            *set |=temp;
+
+        }
+        i++;
+        Emplace(tab, set);
+    }
+    else{
+        i=0;
+    }
+
 }
 
-void Emplace( std::string input, int* a ){
-	if( Counter == 0 )
-		*a = 0;
+void Insert (char* tab, int* set){
 
-	if( Counter < input.length() ){
-		if( input.at( Counter ) == '0' ||  input.at( Counter ) == '1' ){
-			int readSet = 1;
+    if(*(tab+i)=='0' || *(tab+i)=='1' || *(tab+i)==' '){
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 16;
-			Counter = dodaj( Counter, 1 );
+        if(*(tab+i)=='1' || *(tab+i)=='0'){
+            int temp=1;
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 8;
-			Counter = dodaj( Counter, 1 );
+            if(*(tab+i)=='1') temp<<=16;
+            i++;
+            if(*(tab+i)=='1') temp<<=8;
+            i++;
+            if(*(tab+i)=='1') temp<<=4;
+            i++;
+            if(*(tab+i)=='1') temp<<=2;
+            i++;
+            if(*(tab+i)=='1') temp<<=1;
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 4;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 2;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 1;
-
-			*a |= readSet;
-		}
-
-		Counter = dodaj( Counter, 1 );
-		Emplace( input, a );
-	}
-	else
-		Counter = 0;
+            *set |=temp;
+        }
+        i++;
+        Insert(tab, set);
+    }
+    else{
+        i=0;
+    }
 }
 
-void Insert( std::string input, int* a ){
-	if( Counter < input.length() ){
-		if( input.at( Counter ) == '0' ||  input.at( Counter ) == '1' ){
-			int readSet = 1;
+void Erase (char* tab, int* set){
+    if(*(tab+i)=='0' || *(tab+i)=='1' || *(tab+i)==' '){
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 16;
-			Counter = dodaj( Counter, 1 );
+        if(*(tab+i)=='1' || *(tab+i)=='0'){
+            int temp=1;
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 8;
-			Counter = dodaj( Counter, 1 );
+            if(*(tab+i)=='1') temp<<=16;
+            i++;
+            if(*(tab+i)=='1') temp<<=8;
+            i++;
+            if(*(tab+i)=='1') temp<<=4;
+            i++;
+            if(*(tab+i)=='1') temp<<=2;
+            i++;
+            if(*(tab+i)=='1') temp<<=1;
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 4;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 2;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 1;
-
-			*a |= readSet;
-		}
-
-		Counter = dodaj( Counter, 1 );
-		Insert( input, a );
-	}
-	else
-		Counter = 0;
+            temp=~temp;
+            *set &=temp;
+        }
+        i++;
+        Erase(tab, set);
+    }
+    else{
+        i=0;
+    }
 }
 
-void Erase( std::string input, int* a ){
-	if( Counter < input.length() ){
-		if( input.at( Counter ) == '0' ||  input.at( Counter ) == '1' ){
-			int readSet = 1;
+void Print (int set, char* tab){
+    if(set==0){
+        *(tab+0)='e';
+        *(tab+1)='m';
+        *(tab+2)='p';
+        *(tab+3)='t';
+        *(tab+4)='y';
+        *(tab+5)='\0';
+    }
+    else {
+        if (set & (1 << 31)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 30)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 29)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 28)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 27)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 26)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 25)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 24)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 23)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 22)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 21)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 20)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 19)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 18)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 17)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 16)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 15)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 14)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 13)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 12)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 11)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 10)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 9)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 8)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 7)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 6)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 5)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 4)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 3)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='1';
+        }
+        if (set & (1 << 2)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
+            *(tab+(i++))='0';
+        }
+        if (set & (1 << 1)){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='1';
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 16;
-			Counter = dodaj( Counter, 1 );
+        }
+        if (set & 1){
+            if(first==0)
+                *(tab+(i++))=' ';
+            else first=0;
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
+            *(tab+(i++))='0';
 
-			if( input.at( Counter ) == '1' )
-				readSet <<= 8;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 4;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 2;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				readSet <<= 1;
-
-			readSet = ~readSet;
-			*a &= readSet;
-		}
-
-		Counter = dodaj( Counter, 1 );
-		Erase( input, a );
-	}
-	else
-		Counter = 0;
+        }
+        *(tab+i)='\0';
+    }
+    first=1;
+    i=0;
 }
 
-void Print( int a, std::string* output ){
-	if( Display < 0 ){
-		Display = 31;
-		return;
-	}
-
-	if( a == 0 ){
-		Display = 31;
-		*output = "empty";
-		return;
-	}
-
-	if( Display == 31 )
-		*output = "";
-
-	if( a & ( 1 << Display ) ){
-		std::stringstream ss;
-		ss << *output;
-		ss << ( Display / 16 )
-		   << ( Display / 8 ) % 2
-		   << ( Display / 4 ) % 4 % 2
-		   << ( Display / 2 ) % 8 % 2
-		   << ( Display % 2 ) << " ";
-		*output = ss.str();
-	}
-
-	Display = dodaj( Display, dodaj( 1, ~1) );
-	Print( a, output );
+bool Emptiness(int set){
+    return set==0;
 }
 
-bool Emptiness( int a ){
-	return ( a == 0 );
+bool Nonempty(int set){
+    return set!=0;
 }
 
-bool Nonempty( int a){
-	return ( a != 0 );
+bool Member(char* tab, int set){
+
+    if(*(tab+i)=='0' || *(tab+i)=='1' || *(tab+i)==' '){
+
+        if(*(tab+i)=='1' || *(tab+i)=='0'){
+            int temp=1;
+
+            if(*(tab+i)=='1') temp<<=16;
+            i++;
+            if(*(tab+i)=='1') temp<<=8;
+            i++;
+            if(*(tab+i)=='1') temp<<=4;
+            i++;
+            if(*(tab+i)=='1') temp<<=2;
+            i++;
+            if(*(tab+i)=='1') temp<<=1;
+
+            i=0;
+            return set & temp;
+        }
+        i++;
+        Member(tab, set);
+    }
+    else{
+        i=0;
+        return false;
+    }
 }
 
-bool Member( std::string input, int a ){
-	if( Counter < input.length() ){
-		if( input.at( Counter ) == '0' ||  input.at( Counter ) == '1' ){
-			int b = 1;
-
-			if( input.at( Counter ) == '1' )
-				b <<= 16;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				b <<= 8;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				b <<= 4;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				b <<= 2;
-			Counter = dodaj( Counter, 1 );
-
-			if( input.at( Counter ) == '1' )
-				b <<= 1;
-
-			Counter = 0;
-			return ( a & b );
-		}
-
-		Counter = dodaj( Counter, 1 );
-		return Member( input, a );
-	}
-	else{
-		Counter = 0;
-		return 0;
-	}
+bool Disjoint(int set1, int set2){
+    return !(set1 & set2);
 }
 
-bool Disjoint( int a, int b ){
-	return  ( a & b ) == 0;
+bool Conjunctive(int set1, int set2){
+    return set1 & set2;
 }
 
-bool Conjunctive( int a, int b ){
-	return ( a & b ) != 0;
+bool Equality(int set1, int set2){
+    return set1 == set2;
 }
 
-bool Equality( int a, int b ){
-	return ( a == b );
+bool Inclusion(int set1, int set2){
+    return (set1 & set2) == set1;
+
 }
 
-bool Inclusion( int a, int b ){
-	return ( ( a | b ) == b );
+void Union(int set1, int set2, int* set){
+    *set = set1 | set2;
 }
 
-void Union( int a, int b, int* output ){
-	*output = a | b;
+void Intersection (int set1, int set2, int* set){
+    *set= set1 & set2;
 }
 
-void Intersection( int a, int b, int* output ){
-	*output = a & b;
+void Symmetric(int set1, int set2, int* set){
+    *set = set1 ^ set2;
 }
 
-void Symmetric( int a, int b, int* output ){
-	*output = a ^ b;
+void Difference(int set1, int set2, int* set){
+    *set= set1 & (~set2);
 }
 
-void Difference( int a, int b, int* output ){
-	*output = ( a & ( ~b ) );
+void Complement(int set1, int* set){
+    *set=~set1;
 }
 
-void Complement( int a, int* output ){
-	*output = ~a;
+
+int Cardinality(int set){
+    if(i==0){
+        power=0;
+    }
+    if(i<32){
+        int temp=1;
+        temp<<=i;
+        i++;
+        if(set & temp)
+            power++;
+        return Cardinality(set);
+    }
+    i=0;
+    return power;
 }
 
-int policz( int a, int i = 0 ){
-	if( i < 32 ){
-		if( a & ( 1 << i ) ) {
-			return dodaj( policz( a, dodaj( i, 1 ) ), 1 );
-		}
-		return policz( a, dodaj( i, 1 ));
-	}
-
-	return 0;
+bool LessThen(int set1, int set2){
+    return Cardinality(set1)<Cardinality(set2) || (Cardinality(set1)==Cardinality(set2)&& set1<set2);
 }
 
-bool LessThen( int a, int b ){
-	return ( policz( a ) < policz( b ) )
-	    || ( policz( a ) == policz( b )  &&  (unsigned int)a < (unsigned int)b );
+bool LessEqual(int set1, int set2){
+    return Cardinality(set1)<Cardinality(set2) || (Cardinality(set1)==Cardinality(set2)&& set1<=set2);
 }
 
-bool LessEqual( int a, int b ){
-	return ( policz( a ) < policz( b ) )
-	    || ( policz( a ) == policz( b )  &&  (unsigned int)a <= (unsigned int)b );
+bool GreatEqual(int set1, int set2){
+    return Cardinality(set1)>Cardinality(set2) || (Cardinality(set1)==Cardinality(set2)&& set1>=set2);
 }
 
-bool GreatEqual( int a, int b ){
-	return ( policz( a ) > policz( b ) )
-	    || ( policz( a ) == policz( b )  &&  (unsigned int)a >= (unsigned int)b );
-}
-
-bool GreatThen( int a, int b ){
-	return ( policz( a ) > policz( b ) )
-	    || ( policz( a ) == policz( b )  &&  (unsigned int)a > (unsigned int)b );
+bool GreatThen(int set1, int set2){
+    return Cardinality(set1)>Cardinality(set2) || (Cardinality(set1)==Cardinality(set2)&& set1>set2);
 }
 
